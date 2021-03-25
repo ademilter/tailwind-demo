d84a062e41dd18ebfb40d84a765fd8dbf6aedda5
Adem ilter <ademilter@gmail.com>
2021-03-25 10:21:08 +0300

    fix: optional chaining (thx @eserozvataf)

--- components/button/index.js
+++ components/button/index.js
@@ -3,14 +3,7 @@ import styles from './index.module.css'
 
 export default function Button({ appearance, children, ...props }) {
   return (
-    <button
-      className={cn(
-        styles.base,
-        appearance === 'default' ? styles.default : undefined,
-        appearance === 'link' ? styles.link : undefined
-      )}
-      {...props}
-    >
+    <button className={cn(styles.base, styles?.[appearance])} {...props}>
       {children}
     </button>
   )
